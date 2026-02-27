6b968a1d55b46f16326f7f8a7e2e4e205fc5f5cf
Siddharth Chandra <chandrasiddharth975@gmail.com>
2026-02-27 10:02:03 +0530

    Third lecture

---
+++ Lecture_3/Functions.js
@@ -0,0 +1,23 @@
+// function function(name){
+//     statements
+// }
+
+function doWhatever(){
+    alert("Hello Sir, go to hell and dont come back again.");
+}
+
+function sayHello(name,age){
+    document.write("Hello " + name + ", you are " + age + " years old.<br>");
+}
+
+var res= mul(12,30)
+function mul(a,b){
+    return a*b;
+}
+
+document.write(res)
+
+var add2 = (a,b) =>{return a+b};
+
+var res2 = add2(10,20);
+document.write(res2)
